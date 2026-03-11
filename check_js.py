1e22b214d5546ebb363b268dd4a34a5c508bf11b
Ritu Raj <rituraj@Ritus-MacBook-Air.local>
2026-03-11 05:35:32 +0530

    Remove non-backend files

--- check_js.py
+++
@@ -1,6 +0,0 @@
-import re
-with open("app.html", "r") as f:
-    html = f.read()
-scripts = re.findall(r"<script>(.*?)</script>", html, re.DOTALL)
-with open("temp.js", "w") as f:
-    f.write(scripts[0])
